aea3bec37253d7a10b2bad2fa3d75e2e7a0f3796
Shifa <shifa.gadiwale@gmail.com>
2024-03-29 21:45:49 +0530

    ho gaya re baba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [placeList, setPlaceList] = useState({
+    countries: [],
+    states: [],
+    cities: [],
+  });
+  const [placeObj, setPlaceObj] = useState({
+    country: "",
+    state: "",
+    city: "",
+  });
+
+  const handleChange = (event) => {
+    setPlaceObj({ ...placeObj, [event.target.id]: event.target.value });
+  };
+
+  const getCountries = async () => {
+    let response = await fetch(
+      "https://crio-location-selector.onrender.com/countries"
+    );
+    let data = await response.json();
+    //setPlaceList({ ...placeList, [countries]: data });
+    setPlaceList({ ...placeList, countries: data });
+  };
+  const getStates = async () => {
+    //console.log("selected country :", placeObj.country);
+    let response = await fetch(
+      `https://crio-location-selector.onrender.com/country=${placeObj.country}/states`
+    );
+    let data = await response.json();
+    setPlaceList({ ...placeList, states: data });
+  };
+  const getCities = async () => {
+    let response = await fetch(
+      `https://crio-location-selector.onrender.com/country=${placeObj.country}/state=${placeObj.state}/cities`
+    );
+    let data = await response.json();
+    setPlaceList({ ...placeList, cities: data });
+  };
+  useEffect(() => {
+    getCountries();
+  }, []);
+  useEffect(() => {
+    setPlaceObj({ ...placeObj, state: "", city: "" });
+    if (placeObj.country) getStates();
+  }, [placeObj.country]);
+  useEffect(() => {
+    setPlaceObj({ ...placeObj, city: "" });
+    if (placeObj.country && placeObj.state) getCities();
+  }, [placeObj.state]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Select Location</h1>
+      <div className="dropdowns">
+        <select
+          value={placeObj.country}
+          id="country"
+          onChange={handleChange}
+          className="dropdown"
         >
-          Learn React
-        </a>
-      </header>
+          <option value="" disabled>
+            Select Country
+          </option>
+          {placeList.countries.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </select>
+        <select
+          value={placeObj.state}
+          id="state"
+          onChange={handleChange}
+          className="dropdown"
+          disabled={!placeObj.country}
+        >
+          <option value="" disabled>
+            Select State
+          </option>
+          {placeList.states.map((state) => (
+            <option key={state} value={state}>
+              {state}
+            </option>
+          ))}
+        </select>
+        <select
+          value={placeObj.city}
+          id="city"
+          onChange={handleChange}
+          className="dropdown"
+          disabled={!placeObj.state}
+        >
+          <option value="" disabled>
+            Select City
+          </option>
+          {placeList.cities.map((city) => (
+            <option key={city} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="result">
+        {placeObj.city && (
+          <h3>
+            You selected <span className="highlighted">{placeObj.city}</span>,{' '}
+            <span className="dim">{placeObj.country}</span>,{' '}
+            <span className="dim">{placeObj.state}</span>
+          </h3>
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
